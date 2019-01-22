72cd46fa9f75006a7ef4d2cfbec4f84dcace1dc6
areejaa <areej.hsa@gmail.com>
2019-01-22 20:48:34 +0300

    uploadImage

--- app/src/main/java/com/example/illuminate_me/logoScreen.java
+++ app/src/main/java/com/example/illuminate_me/logoScreen.java
@@ -18,31 +18,27 @@ public class logoScreen extends AppCompatActivity {
         //this is part of the onCreate method
         setContentView(R.layout.activity_logo_screen);
         // to hide the action bar
-        getSupportActionBar().hide();
+        //getSupportActionBar().hide();
         logoLauncher logo = new logoLauncher();
         logo.start();
-
     }// end onCreate
 
     // this class purpose is to make the logo screen appears for a number of seconds before showing the main screen
     private class logoLauncher extends Thread {
 
         public void run (){
-
             try {
                 // to make the logo appear for 2 seconds
                 sleep(2000);}
             catch (InterruptedException e){
                 e.printStackTrace();
             }
-
             // to move from this activity to the main activity (main screen)
             Intent intent = new Intent (logoScreen.this, MainActivity.class);
             startActivity(intent);
             // to not go back to this screen
             logoScreen.this.finish();
         }// end run method
-
 
     } // end private class
 }
