90bea52a73e3b4ed5e95af222b34bcf07ef3ec57
Fatimah <Fatimah.sheikho@hotmail.com>
2019-01-26 18:05:39 +0300

    After adding SPEECH method ftm

---
+++ app/src/main/java/com/example/illuminate_me/AudioOutputFormat.java
@@ -0,0 +1,8 @@
+package com.example.illuminate_me;
+
+public class AudioOutputFormat {
+    /// <summary>
+    /// raw-16khz-16bit-mono-pcm request output audio format type.
+    /// </summary>
+    public static final String Raw16Khz16BitMonoPcm = "raw-16khz-16bit-mono-pcm";
+}
